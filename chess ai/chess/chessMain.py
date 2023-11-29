e9149dae542bba7ab9b4bbf04f175e6c5cfa7b9f
Victoria Le <95896163+levictoria0117@users.noreply.github.com>
2023-11-28 16:24:59 -0800

    Update chessMain.py

--- chess ai/chess/chessMain.py
+++ chess ai/chess/chessMain.py
@@ -1,12 +1,12 @@
 """User input and displaying current state of all pieces"""
 
 import pygame as p
-from Chess import ChessEngine
+import chessEngine
 
 WIDTH = 600
 HEIGHT = 600
 DIMENSIONS = 8
-SQUARE = HEIGHT//DIMENSIONS
+SQUARE = HEIGHT/DIMENSIONS
 MAX_FPS = 15
 IMAGES = {}
 
@@ -22,31 +22,39 @@ def main():
     screen = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
-    gameState = ChessEngine.GameState()
+    gameState = chessEngine.state()
     loadingPieces()
     running = True
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+        drawChessGameState(screen)
         clock.tick(MAX_FPS)
         p.display.flip()
         
-def drawChessGameState(screen, gs):
-    drawBoard(screen)
-    drawPieces(screen, gs.board)
+def drawChessGameState(screen):
+    drawChessBoard(screen)
+    drawChessPieces(screen, chessEngine.state().board)
     
 def drawChessBoard(screen):
     colors = [p.Color("white"), p.Color("gray")]
-    for r1 in range[DIMENSIONS]:
-        for r2 in range[DIMENSIONS]:
-            color = colors[((r+c)%2)]
-            p.draw.rect(screen, color, p.Rect(r2*SQUARE, r1*SQUARE, SQUARE, SQUARE))
+    for r in range(DIMENSIONS):
+        for c in range(DIMENSIONS):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQUARE, r*SQUARE, SQUARE, SQUARE))
             
-        
-    
-#def drawChessPieces(screen, gs):
-    
+def drawChessPieces(screen, board):
+    for r in range(DIMENSIONS):
+        for c in range(DIMENSIONS):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQUARE, r*SQUARE, SQUARE, SQUARE)) 
+
+                  
+if __name__ == "__main__":
+    main()
+
     
     
             
