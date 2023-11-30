9145346da4aee0afbc3de09f4dc6b9d4b656157d
Jose Velasco <jose.juarez.velasco@gmail.com>
2023-11-30 15:40:17 -0800

    Adjusted state to State, created move methods

--- chess ai/chess/ai.py
+++ chess ai/chess/ai.py
@@ -1,16 +1,16 @@
 from abc import ABC, abstractclassmethod
 from chessEnums import PieceColor
-from chessEngine import state
+from chessEngine import State
 
 class AI(ABC):
     def __init__(self, team: PieceColor):
         self.team = team
     @abstractclassmethod
-    def move(self, board: state) -> bool:
+    def move(self, board: State) -> bool:
         """
         returns True and makes move found else False and no move made
 
-        :param board: object of state that holds the board that contains current board state
+        :param board: object of State that holds the board that contains current board State
         :return: True if move found else False
         """
         ...
@@ -20,35 +20,35 @@ class MiniMaxAlphaBeta(AI):
         super().__init__(team)
         self.depth = depth
     
-    def move(self, board: state):
+    def move(self, board: State):
         pass
 
-    def minimax(self, board: state) -> str:
+    def minimax(self, board: State) -> str:
         """
-        :param: board object with current board state
+        :param: board object with current board State
         :return: the best move found as a string
         """
         pass
 
-    def min_value(self, board: state, depth: int, team: PieceColor, alpha: int, beta: int) -> tuple[float, str | None]:
+    def min_value(self, board: State, depth: int, team: PieceColor, alpha: int, beta: int) -> tuple[float, str | None]:
         """
-        :param board: state object representing representing a chess board state
+        :param board: State object representing representing a chess board State
         :param depth: minimax depth
         :param team: teaming being evaluated black | white
         :param alpha: alpha-beta pruning, alpha, for minimax algorithm 
         :param beta: alpha-beta pruning, beta, for minimax algorithm
-        :return: return tuple (float value of the state evaluation of board, best move string)
+        :return: return tuple (float value of the State evaluation of board, best move string)
         """
         pass
 
-    def max_value(self, board: state, depth: int, team: PieceColor, alpha: int, beta: int) -> (float, str):
+    def max_value(self, board: State, depth: int, team: PieceColor, alpha: int, beta: int) -> (float, str):
         """
-        :param board: state object representing representing a chess board state
+        :param board: State object representing representing a chess board State
         :param depth: minimax tree depth
         :param team: teaming being evaluated black | white
         :param alpha: alpha-beta pruning, alpha, for minimax algorithm 
         :param beta: alpha-beta pruning, beta, for minimax algorithm
-        :return: return tuple (float value of the state evaluation of board, best move string)
+        :return: return tuple (float value of the State evaluation of board, best move string)
         """
         pass
 
--- chess ai/chess/chessEngine.py
+++ chess ai/chess/chessEngine.py
@@ -1,6 +1,16 @@
-"""Stores information about current state of the board and determines valid moves at that state"""
+from chessEnums import PieceColor
 
-class state():
+class Move:
+    def __init__(self, startSq: tuple[int, int], endSq: tuple[int, int], board: list[list[str]]):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+"""Stores information about current state of the board and determines valid moves at that state"""
+class State():
     def __init__(self):
         self.board = [
             ["b_rook", "b_knight", "b_bishop", "b_queen", "b_king", "b_bishop", "b_knight", "b_rook"],
@@ -14,3 +24,18 @@ class state():
         ]
         self.white_move = True
         self.movelog = []
+    
+    def makeMove(self, move: Move):
+        self.board[move.startRow][move.startCol] == "--"
+        self.board[move.endRow][move.endCol] == move.pieceMoved
+        self.movelog.append(move)
+        self.white_move = not self.white_move
+    
+    def get_all_allowed_moves(self, team: PieceColor):
+        """
+        :param team: team to enumerate all moves
+        : return: list of all allowed moves for team
+        """
+        pass
+
+    
